4e714565a4f09f682ecbb3aaab6301094a7ede6f
= <=>
2024-09-19 14:48:08 +1000

    everything

---
+++ Alignment.py
@@ -0,0 +1,96 @@
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import Any, Optional
+import pygame
+
+
+class Position(Enum):
+    '''Sequence of values for positional locations'''
+    none = "none"
+    top = "top"
+    centre = "centre"
+    bottom = "bottom"
+    bottom_left = "bottom left"
+
+@dataclass
+class Coords:
+    x: float|None
+    y: float|None
+
+    def __getitem__(self, index: int) -> float|None:
+        match index:
+            case 0:
+                return self.x
+            case 1:
+                return self.y
+            case _:
+                raise IndexError
+            
+    def __setitem__(self, index: int, value: float) -> None:
+        match index:
+            case 0:
+                self.x = value
+            case 1:
+                self.y = value
+            case _:
+                raise IndexError
+            
+    def __repr__(self) -> str:
+        return str((self.x,self.y))
+
+
+class Alignment:
+
+    def __init__(self, position: Position = Position.none, offsets: tuple[float, float] = (0,0), overwrites: tuple[Optional[float], Optional[float]] = (None, None)):
+        self.position = position
+        self.offsets = Coords(offsets[0], offsets[1])
+        self.overwrites = Coords(overwrites[0], overwrites[1])
+
+    def get_coords(self, object, screen) -> tuple[float, float]:
+        match self.position:
+            case Position.none:
+                x, y = 0.0, 0.0
+            case Position.top:
+                x = self.centre_position(object, screen)[0]
+                y = 0
+            case Position.centre:
+                x, y = self.centre_position(object, screen)
+            case Position.bottom_left:
+                x = 0
+                y = self.bottom_y(object, screen)
+            case Position.bottom:
+                x = self.centre_position(object, screen)[0]
+                y = self.bottom_y(object, screen)
+
+        x = x + self.offsets.x if self.offsets.x is not None else 0
+        y = y + self.offsets.y if self.offsets.y is not None else 0
+
+        if self.overwrites.x is not None:
+            x = self.overwrites.x
+
+        if self.overwrites.y is not None:
+            y = self.overwrites.y
+
+        return (x, y)
+    
+    def centre_position(self, object: pygame.Surface, screen: pygame.Surface) -> tuple[float, float]: 
+        x = (screen.get_width() - object.get_width()) / 2
+        y = (screen.get_height() - object.get_height()) / 2
+        return (x, y)
+    
+    def bottom_y(self, object: pygame.Surface, screen: pygame.Surface) -> float:
+        y = screen.get_height() - 2*object.get_height()
+        return y
+    
+    def get_offsets(self):
+        return self.offsets
+    
+    def set_offsets(self, values: Coords):
+        self.offsets = values
+
+    def get_overwrites(self):
+        return self.overwrites
+    
+    def set_overwrites(self, values: Coords):
+        self.overwrites = values
---
+++ TextDisplay.py
@@ -0,0 +1,102 @@
+
+
+from dataclasses import dataclass
+from typing import Callable, Optional, TypeVar, Union
+from .Alignment import Alignment
+import pygame
+
+type coordsOrAlignment = tuple[float, float] | Callable[[pygame.Surface, pygame.Surface], tuple[float, float]]
+
+T = TypeVar("T")
+def optionalUnpack(value: Optional[T], default: T) -> T:
+    if value is None:
+        return default
+    else:
+        return value
+
+class TextDisplay:
+    '''A collection of helper functions for displaying messages in pygame'''
+
+    def __init__(self, screen: pygame.Surface, font: pygame.font.Font, colour: tuple):
+        self.screen = screen
+        self.font = font
+        self.colour = colour
+
+
+    def displayLine(self, message: str, alignment: Alignment, new_font: Optional[pygame.font.Font] = None, new_colour: Optional[tuple] = None) -> Callable[[], None]:
+        '''Returns callable that displays single line of text on screen'''
+        font: pygame.font.Font = optionalUnpack(new_font, self.font)
+        colour: tuple = optionalUnpack(new_colour, self.colour)
+        @dataclass
+        class Talker:
+            message: str
+            alignment: Alignment
+            screen: pygame.Surface
+            font: pygame.font.Font
+            colour: tuple
+
+            def talk(self) -> None:
+                line = self.font.render(self.message, True, self.colour)
+                start_position = self.alignment.get_coords(line, self.screen)
+                self.screen.blit(line,start_position)
+        return Talker(message, alignment, self.screen, font, colour).talk
+
+
+    def displayMultipleLines(self, message: list[str], alignment: Alignment, new_font: Optional[pygame.font.Font] = None, new_colour: Optional[tuple] = None) -> Callable[[], None]:
+        '''Returns function that displays multiple lines of text cascaddown a window. Use if displayLine() is going off the screen'''
+        font: pygame.font.Font = optionalUnpack(new_font, self.font)
+        colour: tuple = optionalUnpack(new_colour, self.colour)
+        @dataclass
+        class Talker:
+            message: list[str]
+            alignment: Alignment
+            screen: pygame.Surface
+            font: pygame.font.Font
+            colour: tuple
+
+            def talk(self) -> None:
+                line_no = 0
+                diff = int(self.screen.get_width() * 0.8/self.font.size('a')[0])
+                height = self.font.get_height()
+                for item in self.message:
+                    strings = [item[start:start+diff] for start in range(0, len(item), diff)]
+                    for segment in strings:
+                        line = self.font.render(segment, True, self.colour)
+                        start_position = self.alignment.get_coords(line, self.screen)
+                        self.screen.blit(line,(start_position[0], start_position[1] + height*line_no))
+                        line_no+=1
+        return Talker(message, alignment, self.screen, font, colour).talk
+                
+
+    def request_prompt(self, message: str, alignment: Alignment, new_font: Optional[pygame.font.Font] = None, new_colour: Optional[tuple] = None) -> Callable[[str], None]:
+        '''Returns callable that displays one line with option to add more chrs onto end'''
+        font: pygame.font.Font = optionalUnpack(new_font, self.font)
+        colour: tuple = optionalUnpack(new_colour, self.colour)
+        @dataclass
+        class Talker:
+            message: str
+            alignment: Alignment
+            screen: pygame.Surface
+            font: pygame.font.Font
+            colour: tuple
+
+            def talk(self, add: str) -> None:
+                line = self.font.render(self.message + add, True, self.colour)
+                start_position = self.alignment.get_coords(line, self.screen)
+                self.screen.blit(line,start_position)
+        return Talker(message, alignment, self.screen, font, colour).talk
+    
+
+    def image_display(self, image: pygame.Surface, alignment: Alignment, scale: tuple) -> Callable[[], None]:
+        '''Returns callable that displays image at specified position oecified size'''
+        @dataclass
+        class Painter:
+            image: pygame.Surface
+            alignment: Alignment
+            screen: pygame.Surface  
+            scale: tuple
+          
+            def paint(self):
+                start_position = self.alignment.get_coords(self.image, self.screen)
+                self.screen.blit(pygame.transform.scale(image, scale), start_position)
+        return Painter(image, alignment, self.screen, scale).paint
