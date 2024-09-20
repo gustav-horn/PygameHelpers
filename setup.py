7699d4c0b6b60b6a517d909137fff4d5e33b6bed
gustav-horn <158514285+gustav-horn@users.noreply.github.com>
2024-09-20 11:20:24 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup
 
+setup(
+   name='PygameHelpers',
+   version='1.0',
+   description='Collection of classes to help with pygame',
+   author='Gustav Horn',
+   author_email='mbkcheah@gmail.com',
+   packages=['PygameHelpers'],  #same as name
+   install_requires=['pygame'], #external packages as dependencies
+)
